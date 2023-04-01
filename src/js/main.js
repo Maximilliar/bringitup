60fea621cb484a76a69e5047d139e57ca6774f9a
Maxim Makarenko <maximmakar@gmail.com>
2023-04-01 16:23:14 +0600

    Fix code, add new functions v 1.8

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import VideoPlayer from './modules/playVideo';
 import MiniSlider from './modules/slider/slider-mini';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -43,7 +45,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new VideoPlayer('.showup .play', '.overlay').init();
     new VideoPlayer('.module__video-item .play', '.overlay').init();
+    new VideoPlayer('.schedule__info .play', '.overlay').init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 }); 
